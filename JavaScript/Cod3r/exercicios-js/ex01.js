864e570c64ce46dc9eb2f8e2da7b692e86cd9b31
Isaque Resende de Freitas <isaquerf@gmail.com>
2023-11-09 16:10:52 +0000

    fiz os exercícios 1 e 2 de fundamentos do Cod3r

---
+++ JavaScript/Cod3r/exercicios-js/ex01.js
@@ -0,0 +1,8 @@
+/* 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.*/
+
+function calc(a, b){
+    console.log(a + b, a - b, a, a / b)
+}
+
+calc(2, 2)
